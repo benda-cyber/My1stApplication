acf28c81b970c73993177895a7d51577abee7f80
benda-cyber <gbdxxx1@gmail.com>
2020-12-29 21:10:53 +0200

    added login partially

--- app/src/main/java/com/example/my1stapplication/MainActivity3.java
+++ app/src/main/java/com/example/my1stapplication/MainActivity3.java
@@ -1,26 +1,65 @@
 package com.example.my1stapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity3 extends AppCompatActivity {
+
     private FirebaseAuth mAuth;
+    EditText emailText;
+    EditText passText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        emailText = findViewById(R.id.emailText);
+        passText = findViewById(R.id.passwordText);
 
         mAuth = FirebaseAuth.getInstance();
     }
+
 
     public void register(View view) {
     }
 
     public void login(View view) {
+
+        String email = emailText.getText().toString();
+        String password = passText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                });
     }
 
 
