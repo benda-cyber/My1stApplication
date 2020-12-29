32cef127f8198c12e96f8012ea063bee7400adfd
benda-cyber <gbdxxx1@gmail.com>
2020-12-29 20:46:00 +0200

    changed login page and started button functions

--- app/src/main/java/com/example/my1stapplication/MainActivity3.java
+++ app/src/main/java/com/example/my1stapplication/MainActivity3.java
@@ -3,12 +3,25 @@ package com.example.my1stapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    public void register(View view) {
+    }
+
+    public void login(View view) {
+    }
+
+
 }
